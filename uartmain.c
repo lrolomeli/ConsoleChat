e36f3f8da4f3d6b8c3e44de91ab93668e76b2b13
Luis Roberto <ie700093@iteso.mx>
2018-03-10 23:34:16 -0600

    trying to protect uart transmission and reception from different tasks with mutex, still not working but echo task is working fine

--- uartmain.c
+++ uartmain.c
@@ -37,6 +37,9 @@
 
 #include "pin_mux.h"
 #include "clock_config.h"
+#include "freeRTOS.h"
+#include "task.h"
+#include "semphr.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -57,6 +60,7 @@ void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, 
  * Variables
  ******************************************************************************/
 uart_handle_t g_uartHandle;
+SemaphoreHandle_t tx_semaphore;
 
 uint8_t g_tipString[] =
     "\r\nYou have entered a valid value\r\n";
@@ -74,15 +78,112 @@ volatile bool rxOnGoing = false;
 /*!
  * @brief Main function
  */
+#if debug_print_task_mutex_sem
+void uart_transmitter_task(void * pvParameters)
+{
+	/**
+	 * Cuando queramos mandar un mensaje por pantalla
+	 * es necesario despertar esta funcion por medio de un give
+	 * al semaforo tx_semaphore
+	 */
+	static uart_transfer_t xfer;
+	/* Send g_tipString out. */
+	xfer.data = g_tipString;
+	xfer.dataSize = sizeof(g_tipString) - 1;
+	volatile bool first_time = true;
+
+	for(;;)
+	{
+		/* example_printf. */
+		if(first_time)//si se quiere imprimir se manda a la funcion un booleano casteado
+		{
+			xSemaphoreTake(tx_semaphore, portMAX_DELAY);
+			txOnGoing = true;   //EL DATO ESTA LISTO PARA SER ENVIADO
+			UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &xfer);
+			first_time = false;
+		}
+
+	}
+
+}
+#endif
+
+void uart_transceiver_task(void * pvParameters)
+{
+	uart_transfer_t sendXfer;
+	uart_transfer_t receiveXfer;
+	static uint8_t phrase_size = 0;
+	static uint8_t phrase[50] =
+	{ 0 };
+
+    /* Start to echo. */
+    sendXfer.data = &g_txBuffer;
+    sendXfer.dataSize = ECHO_BUFFER_LENGTH;
+    receiveXfer.data = &g_rxBuffer;
+    receiveXfer.dataSize = ECHO_BUFFER_LENGTH;
+
+	for(;;)
+	{
+		/* If RX is idle and g_rxBuffer is empty, start to read data to g_rxBuffer. */
+		    /* Si no se encuentra recibiendo y el buffer de recepcion esta vacio. */
+		    if ((!rxOnGoing) && rxBufferEmpty)
+		    {
+		        /**entonces puedes decir que ahora ya se encuentra recibiendo*/
+		        rxOnGoing = true;
+		        /**puedes empezar a recibir*/
+		        UART_TransferReceiveNonBlocking(DEMO_UART, &g_uartHandle, &receiveXfer, NULL);
+		    }
+
+		    /* If TX is idle and g_txBuffer is full, start to send data. */
+		    /* Si no se encuentra transmitiendo y el buffer de transmision esta lleno. */
+		    if ((!txOnGoing) && txBufferFull)
+		    {
+
+		    	if('\r' != *(sendXfer.data) && '\b' != *(sendXfer.data) && phrase_size < 50)
+		    	{
+		    		*(phrase + phrase_size) = *(sendXfer.data);
+		    		phrase_size++;
+		    	}
+		    	else if('\b' == *(sendXfer.data) && phrase_size < 50)
+		    	{
+		    		phrase_size = (phrase_size > 0) ? phrase_size - 1 : 0;
+		    	}
+		    	else if('\r' == *(sendXfer.data) && phrase_size < 50)
+		    	{
+		    		phrase_size = 0;
+		    		//guardar frase y utilizarla
+		    	}
+		    	else
+		    	{
+		    		phrase_size = 0;
+		    	}
+
+		        /**entonces ya se encuentra transmitiendo*/
+		        txOnGoing = true;
+		        /**comienza a transmitir lo que hay en el buffer*/
+		        UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &sendXfer);
+
+		    }
+
+		    /* If g_txBuffer is empty and g_rxBuffer is full, copy g_rxBuffer to g_txBuffer. */
+		    /* Si el buffer de recepcion no esta vacio y el buffer de transmision tampoco. */
+		    if ((!rxBufferEmpty) && (!txBufferFull))
+		    {
+		        /**entonces copia lo que hay en el buffer de recepcion al de transmision*/
+		        memcpy(&g_txBuffer, &g_rxBuffer, ECHO_BUFFER_LENGTH);
+		        /**el buffer de recepcion esta vacio*/
+		        rxBufferEmpty = true;
+		        /**el buffer de transmision esta lleno*/
+		        txBufferFull = true;
+		    }
+	}
+
+
+}
+
 int main(void)
 {
     uart_config_t config;
-    uart_transfer_t xfer;
-    uart_transfer_t sendXfer;
-    uart_transfer_t receiveXfer;
-    static uint8_t phrase[50] = {0};
-    static uint8_t i;
-
 
     BOARD_InitPins();
     BOARD_BootClockRUN();
@@ -94,75 +195,21 @@ int main(void)
     UART_Init(DEMO_UART, &config, DEMO_UART_CLK_FREQ);
     UART_TransferCreateHandle(DEMO_UART, &g_uartHandle, UART_UserCallback, NULL);
 
-    /* Send g_tipString out. */
-    xfer.data = g_tipString;
-    xfer.dataSize = sizeof(g_tipString) - 1;
+    tx_semaphore = xSemaphoreCreateMutex();
 
-#if example_printf
-    txOnGoing = true;   //EL DATO ESTA LISTO PARA SER ENVIADO
-    UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &xfer);
+	xTaskCreate(uart_transceiver_task, "echo_task", configMINIMAL_STACK_SIZE, NULL,
+	        configMAX_PRIORITIES, NULL);
 
-    /* Wait send finished */
-    while (txOnGoing)
-    {
-    }
+#if debug_print_task_mutex_sem
+	xTaskCreate(uart_transmitter_task, "print_task", configMINIMAL_STACK_SIZE, NULL,
+	        configMAX_PRIORITIES - 1, NULL);
 #endif
 
-    /* Start to echo. */
-    sendXfer.data = &g_txBuffer;
-    sendXfer.dataSize = ECHO_BUFFER_LENGTH;
-    receiveXfer.data = &g_rxBuffer;
-    receiveXfer.dataSize = ECHO_BUFFER_LENGTH;
+	vTaskStartScheduler();
 
-    while (1)
+    for(;;)
     {
-        /* If RX is idle and g_rxBuffer is empty, start to read data to g_rxBuffer. */
-        /* Si no se encuentra recibiendo y el buffer de recepcion esta vacio. */
-        if ((!rxOnGoing) && rxBufferEmpty)
-        {
-            /**entonces puedes decir que ahora ya se encuentra recibiendo*/
-            rxOnGoing = true;
-            /**puedes empezar a recibir*/
-            UART_TransferReceiveNonBlocking(DEMO_UART, &g_uartHandle, &receiveXfer, NULL);
-        }
 
-        /* If TX is idle and g_txBuffer is full, start to send data. */
-        /* Si no se encuentra transmitiendo y el buffer de transmision esta lleno. */
-        if ((!txOnGoing) && txBufferFull)
-        {
-        	*(phrase) = *(sendXfer.data);
-			switch (phrase[i]) {
-			case '\r':
-				UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &xfer);
-
-				/* Wait send finished */
-				while (txOnGoing) {
-				}
-				break;
-			case '\0':
-				break;
-			default:
-				break;
-			}
-			i++;
-
-            /**entonces ya se encuentra transmitiendo*/
-            txOnGoing = true;
-            /**comienza a transmitir lo que hay en el buffer*/
-            UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &sendXfer);
-        }
-
-        /* If g_txBuffer is empty and g_rxBuffer is full, copy g_rxBuffer to g_txBuffer. */
-        /* Si el buffer de recepcion no esta vacio y el buffer de transmision tampoco. */
-        if ((!rxBufferEmpty) && (!txBufferFull))
-        {
-            /**entonces copia lo que hay en el buffer de recepcion al de transmision*/
-            memcpy(&g_txBuffer, &g_rxBuffer, ECHO_BUFFER_LENGTH);
-            /**el buffer de recepcion esta vacio*/
-            rxBufferEmpty = true;
-            /**el buffer de transmision esta lleno*/
-            txBufferFull = true;
-        }
     }
 }
 
