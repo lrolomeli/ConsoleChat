e33faa77466c22934ebe8c792ab7096fc7abcea3
Luis Roberto <ie700093@iteso.mx>
2018-03-14 09:35:36 -0600

    uart bluetooth module progress, uart functions generic, and uart print non blocking function implemented

--- uartmain.c
+++ uartmain.c
@@ -43,6 +43,15 @@
 #include "semphr.h"
 #include "event_groups.h"
 
+typedef struct {
+
+	UART_Type * xuart;
+	uart_handle_t uart_handle;
+	uint8_t rx_event;
+	uint8_t tx_event;
+
+} uart_parameters_type;
+
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -55,6 +64,10 @@
 
 #define EVENT_RX (1 << 0)
 #define EVENT_TX (1 << 1)
+
+#define EVENT_BT_RX (1 << 2)
+#define EVENT_BT_TX (1 << 3)
+
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
@@ -62,10 +75,16 @@
 /* UART user callback */
 void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, void *userData);
 
+void UART_UserCallback1(UART_Type *base, uart_handle_t *handle, status_t status, void *userData);
+
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 uart_handle_t g_uartHandle;
+uart_parameters_type cpu;
+
+uart_handle_t g_uartHandle1;
+
 SemaphoreHandle_t tx_semaphore;
 EventGroupHandle_t uart_events_g;
 
@@ -74,10 +93,7 @@ uint8_t g_tipString[] =
 
 uint8_t g_txBuffer = 0;
 uint8_t g_rxBuffer = 0;
-volatile bool rxBufferEmpty = true;
-volatile bool txBufferFull = false;
-volatile bool txOnGoing = false;
-volatile bool rxOnGoing = false;
+
 
 /*******************************************************************************
  * Code
@@ -85,7 +101,7 @@ volatile bool rxOnGoing = false;
 /*!
  * @brief Main function
  */
-#if debug_print_task_mutex_sem
+
 void uart_transmitter_task(void * pvParameters)
 {
 	/**
@@ -94,31 +110,39 @@ void uart_transmitter_task(void * pvParameters)
 	 * al semaforo tx_semaphore
 	 */
 	static uart_transfer_t xfer;
+	uart_parameters_type * uart_param = (uart_parameters_type *) pvParameters;
 	/* Send g_tipString out. */
 	xfer.data = g_tipString;
 	xfer.dataSize = sizeof(g_tipString) - 1;
+
+#if debug_print_task_mutex_sem
 	volatile bool first_time = true;
+#endif
 
 	for(;;)
 	{
+#if debug_print_task_mutex_sem
 		/* example_printf. */
 		if(first_time)//si se quiere imprimir se manda a la funcion un booleano casteado
 		{
-			xSemaphoreTake(tx_semaphore, portMAX_DELAY);
-			txOnGoing = true;   //EL DATO ESTA LISTO PARA SER ENVIADO
-			UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &xfer);
 			first_time = false;
 		}
+#endif
+	    	UART_TransferSendNonBlocking(uart_param->xuart, &(uart_param->uart_handle), &xfer);
+	    	xEventGroupWaitBits(uart_events_g, uart_param->tx_event, pdTRUE, pdTRUE, portMAX_DELAY);
+	    	vTaskDelete(NULL);
 
 	}
 
 }
-#endif
+
 
 void uart_transceiver_task(void * pvParameters)
 {
 	uart_transfer_t sendXfer;
 	uart_transfer_t receiveXfer;
+	uart_parameters_type * uart_param = (uart_parameters_type *) pvParameters;
+
 //	static uint8_t phrase_size = 0;
 //	static uint8_t phrase[50] =
 //	{ 0 };
@@ -129,14 +153,17 @@ void uart_transceiver_task(void * pvParameters)
     receiveXfer.data = &g_rxBuffer;
     receiveXfer.dataSize = ECHO_BUFFER_LENGTH;
 
+
+
     for(;;)
     {
-    	UART_TransferReceiveNonBlocking(DEMO_UART, &g_uartHandle, &receiveXfer, NULL);
-    	xEventGroupWaitBits(uart_events_g, EVENT_RX, pdTRUE, pdTRUE, portMAX_DELAY);
+    	UART_TransferReceiveNonBlocking(uart_param->xuart, &(uart_param->uart_handle), &receiveXfer, NULL);
+    	xEventGroupWaitBits(uart_events_g, uart_param->rx_event, pdTRUE, pdTRUE, portMAX_DELAY);
     	g_txBuffer = g_rxBuffer;
-    	UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &sendXfer);
-    	xEventGroupWaitBits(uart_events_g, EVENT_TX, pdTRUE, pdTRUE, portMAX_DELAY);
+    	UART_TransferSendNonBlocking(uart_param->xuart, &(uart_param->uart_handle), &sendXfer);
+    	xEventGroupWaitBits(uart_events_g, uart_param->tx_event, pdTRUE, pdTRUE, portMAX_DELAY);
     }
+
 
 //	for(;;)
 //	{
@@ -201,6 +228,22 @@ int main(void)
 {
     uart_config_t config;
 
+    uart_config_t config1;
+
+
+
+	uart_parameters_type bluetooth;
+
+	cpu.rx_event = EVENT_RX;
+	cpu.tx_event = EVENT_TX;
+	cpu.uart_handle = g_uartHandle;
+	cpu.xuart = UART0;
+
+	bluetooth.rx_event = EVENT_BT_RX;
+	bluetooth.tx_event = EVENT_BT_TX;
+	bluetooth.uart_handle = g_uartHandle1;
+	bluetooth.xuart = UART1;
+
     BOARD_InitPins();
     BOARD_BootClockRUN();
 
@@ -210,27 +253,49 @@ int main(void)
     CLOCK_EnableClock(kCLOCK_PortB);
     CLOCK_EnableClock(kCLOCK_Uart0);
 
+    CLOCK_EnableClock(kCLOCK_PortC);
+    CLOCK_EnableClock(kCLOCK_Uart1);
 
 	PORT_SetPinMux(PORTB, 10, kPORT_MuxAlt3);
     PORT_SetPinMux(PORTB, 11, kPORT_MuxAlt3);
+
+	PORT_SetPinMux(PORTC, 3, kPORT_MuxAlt3);
+    PORT_SetPinMux(PORTC, 4, kPORT_MuxAlt3);
+
 
     UART_GetDefaultConfig(&config);
     config.enableTx = true;
     config.enableRx = true;
 
     UART_Init(DEMO_UART, &config, DEMO_UART_CLK_FREQ);
-    UART_TransferCreateHandle(DEMO_UART, &g_uartHandle, UART_UserCallback, NULL);
-	xTaskCreate(uart_transceiver_task, "echo_task", configMINIMAL_STACK_SIZE, NULL,
+    UART_TransferCreateHandle(DEMO_UART, &cpu.uart_handle, UART_UserCallback, NULL);
+
+	xTaskCreate(uart_transceiver_task, "echo_task", configMINIMAL_STACK_SIZE, (void *) &cpu,
 	        configMAX_PRIORITIES, NULL);
 
-//#if debug_print_task_mutex_sem
-//	xTaskCreate(uart_transmitter_task, "print_task", configMINIMAL_STACK_SIZE, NULL,
-//	        configMAX_PRIORITIES - 1, NULL);
-//#endif
+
+    UART_GetDefaultConfig(&config1);
+    config1.enableTx = true;
+    config1.enableRx = true;
+    config1.baudRate_Bps = 9600;
+
+    UART_Init(UART1, &config1, CLOCK_GetFreq(UART1_CLK_SRC));
+    UART_TransferCreateHandle(UART1, &bluetooth.uart_handle, UART_UserCallback1, NULL);
+
+	xTaskCreate(uart_transceiver_task, "bluetooth", configMINIMAL_STACK_SIZE, (void *) &bluetooth,
+	        configMAX_PRIORITIES-1, NULL);
+
+	xTaskCreate(uart_transmitter_task, "print_task", 110, (void *) &cpu,
+	        configMAX_PRIORITIES - 2, NULL);
+
 
 	uart_events_g = xEventGroupCreate();
     NVIC_EnableIRQ(UART0_RX_TX_IRQn);
     NVIC_SetPriority(UART0_RX_TX_IRQn, 5);
+
+    NVIC_EnableIRQ(UART1_RX_TX_IRQn);
+    NVIC_SetPriority(UART1_RX_TX_IRQn, 6);
+
 	vTaskStartScheduler();
 
     for(;;)
@@ -259,3 +324,24 @@ void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, 
     }
     portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
 }
+
+void UART_UserCallback1(UART_Type *base, uart_handle_t *handle, status_t status, void *userData)
+{
+    userData = userData;
+
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    if (kStatus_UART_TxIdle == status)
+    {
+    	//event group set bits from isr for tx
+    	xEventGroupSetBitsFromISR(uart_events_g, EVENT_BT_TX, &xHigherPriorityTaskWoken);
+    }
+
+    if (kStatus_UART_RxIdle == status)
+    {
+    	//event group set bits from isr for rx
+    	xEventGroupSetBitsFromISR(uart_events_g, EVENT_BT_RX, &xHigherPriorityTaskWoken);
+    }
+    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+}
+
